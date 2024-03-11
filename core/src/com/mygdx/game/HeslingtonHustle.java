9d8b4749cb9309932bbc5bba987bec21f215e23d
CharlieM312 <cm2339@york.ac.uk>
2024-03-11 12:34:26 +0000

    Added all necessary places

--- core/src/com/mygdx/game/HeslingtonHustle.java
+++ core/src/com/mygdx/game/HeslingtonHustle.java
@@ -8,14 +8,17 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class HeslingtonHustle extends ApplicationAdapter {
 	SpriteBatch batch;
 	private Texture house;
+	private Texture study;
+	private Texture lake;
 	private Texture food;
-	private Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		house = new Texture("house.jpg");
-		food = new Texture("placetoeat.jpg");
+		study = new Texture("placetoeat.jpg");
+		lake = new Texture("lake.jpg");
+		food = new Texture("glasshouse.jpg");
 
 	}
 
@@ -23,8 +26,10 @@ public class HeslingtonHustle extends ApplicationAdapter {
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(food, 120, 150);
+		batch.draw(study, 120, 150);
 		batch.draw(house, 12, 12);
+		batch.draw(lake, 400, 300);
+		batch.draw(food, 250, 250);
 		batch.end();
 	}
 	
@@ -32,6 +37,8 @@ public class HeslingtonHustle extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		house.dispose();
+		study.dispose();
+		lake.dispose();
 		food.dispose();
 	}
 }
