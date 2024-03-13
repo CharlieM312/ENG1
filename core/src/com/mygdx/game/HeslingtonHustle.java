24edac5e0e6f3a424ab37a494f1fa1bf2e7000cc
Max Sweetman <msweetman99@gmail.com>
2024-03-13 15:54:38 +0000

    Create state enumerations for player class which specify which state the player is in

--- core/src/com/mygdx/game/HeslingtonHustle.java
+++ core/src/com/mygdx/game/HeslingtonHustle.java
@@ -4,24 +4,27 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.game.Player;
 
 public class HeslingtonHustle extends ApplicationAdapter {
-	SpriteBatch batch;
+	private SpriteBatch batch;
 	private Texture house;
 	private Texture study;
 	private Texture lake;
 	private Texture food;
 	private String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-	private String CurrentDay;
-	private int ActivityCounter;
-	private int FoodCounter;
-	private int DayCounter;
+	private String currentDay;
+	private int activityCounter;
+	private int foodCounter;
+	private int dayCounter;
 	BitmapFont font;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+
 		house = new Texture("house.jpg");
 		study = new Texture("placetoeat.jpg");
 		lake  = new Texture("lake.jpg");
@@ -29,12 +32,12 @@ public class HeslingtonHustle extends ApplicationAdapter {
 		font  = new BitmapFont();
 
 
-		CurrentDay = days[DayCounter];
-		DayCounter = 0;
-		FoodCounter = 0;
-		ActivityCounter = 0;
-		DayCounter = 0;
-		CurrentDay = days[DayCounter];
+		currentDay = days[dayCounter];
+		dayCounter = 0;
+		foodCounter = 0;
+		activityCounter = 0;
+		dayCounter = 0;
+		currentDay = days[dayCounter];
 	}
 
 	@Override
@@ -45,54 +48,54 @@ public class HeslingtonHustle extends ApplicationAdapter {
 		batch.draw(house, 24, 24);
 		batch.draw(lake, 700, 450);
 		batch.draw(food, 250, 250);
-		font.draw(batch, "Activities completetd: " + ActivityCounter, 800, 580);
-		font.draw(batch, "Times Eaten: " + FoodCounter, 800, 560);
-		font.draw(batch, "Day: " + CurrentDay, 10, 580);
+		font.draw(batch, "Activities completetd: " + activityCounter, 800, 580);
+		font.draw(batch, "Times Eaten: " + foodCounter, 800, 560);
+		font.draw(batch, "Day: " + currentDay, 10, 580);
 		font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
 		batch.end();
 	}
 
 	public int getDayCount()
 	{
-		return DayCounter;
+		return dayCounter;
 	}
 
 	public int getFoodCount() {
-		return FoodCounter;
+		return foodCounter;
 	}
 
 	public int getActivityCount() {
-		return ActivityCounter;
+		return activityCounter;
 	}
 
 	public void IncrementDayCount() {
-		DayCounter++;
+		dayCounter++;
 	}
 
 	public void IncrementFoodCount() {
-		FoodCounter++;
+		foodCounter++;
 	}
 
 	public void IncrementActivityCount() {
-		ActivityCounter++;
+		activityCounter++;
 	}
 
 	public String getCurrentDayName() {
-		return days[DayCounter];
+		return days[dayCounter];
 	}
 
 	public void UpdateCurrentDay(){
 		IncrementDayCount();
-		CurrentDay = days[DayCounter];
+		currentDay = days[dayCounter];
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		batch.draw(study, 340, 150);
 		batch.draw(house, 24, 24);
 		batch.draw(lake, 700, 450);
 		batch.draw(food, 250, 250);
-		font.draw(batch, "Activities completetd: " + ActivityCounter, 800, 580);
-		font.draw(batch, "Times Eaten: " + FoodCounter, 800, 560);
-		font.draw(batch, "Day: " + CurrentDay, 10, 580);
+		font.draw(batch, "Activities completetd: " + activityCounter, 800, 580);
+		font.draw(batch, "Times Eaten: " + foodCounter, 800, 560);
+		font.draw(batch, "Day: " + currentDay, 10, 580);
 		font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
 		batch.end();
 	}
--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -1,12 +1,15 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.math.Rectangle;
 
-public class Player extends Actor {
-    public Player(Vector2 startPosition) {
-        super();
-        
-    }
-    
+public class Player {
+    public enum State {
+        IDLE,
+        MOVING,
+        DOING_RECREATIONAL_ACTIVITY,
+        STUDYING,
+        SLEEPING
+    }  
 }
+
