5703f55edfd326fc61a07e127474307b542fddc9
Max Sweetman <msweetman99@gmail.com>
2024-03-13 17:00:45 +0000

    Create player getter methods

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.graphics.Texture;
 
 public class Player {
     public enum State {
@@ -19,23 +20,59 @@ public class Player {
         LEFT
     }
 
+    private Texture idleTexture
+            , walkingUpTexture
+            , walkingRightTexture
+            , walkingLeftTexture;
+            
     static final float SPEED = 2f;
-    static final float SIZE  = 0.5f;
+    static final float SIZE  = 64;
 
     // Player position within the map
-    Vector2 position = new Vector2();
+    private Vector2 position;
     // Used to calculate the speed that the player moves
-    Vector2 velocity = new Vector2();
+    private Vector2 velocity;
     // Bounding box to detect player collision
-    Rectangle bounds = new Rectangle();
+    private Rectangle bounds;
 
-    State currentState = State.IDLE;
-    Direction currentDirection = Direction.LEFT;
+    private State currentState;
+    private Direction currentDirection;
 
     public Player(Vector2 position) {
+        idleTexture = new Texture("player_idle_texture.png");
         this.position = position;
-        this.bounds.height = SIZE;
-        this.bounds.width = SIZE;
+        bounds.height = SIZE;
+        bounds.width = SIZE;
+        currentState = State.IDLE;
+        currentDirection = Direction.LEFT;
     }
+<<<<<<< Updated upstream
+=======
+    
+    public Texture GetIdleTexture()
+    {
+        return idleTexture;
+    }
+
+    public Vector2 GetPosition()
+    {
+        return position;
+    }
+
+    public Rectangle GetBounds()
+    {
+        return bounds;
+    }
+
+    public State GetCurrentState()
+    {
+        return currentState;
+    }
+
+    public Direction GetCurrentDirection()
+    {
+        return currentDirection;
+    }
+>>>>>>> Stashed changes
 }
 
