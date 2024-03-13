23ea66cd9d0f7a41d65bab4ac1beecdb3d61c821
Max Sweetman <msweetman99@gmail.com>
2024-03-13 16:13:36 +0000

    Create player class members and constructor

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -11,5 +11,31 @@ public class Player {
         STUDYING,
         SLEEPING
     }  
+
+    public enum Direction {
+        UP,
+        RIGHT,
+        DOWN,
+        LEFT
+    }
+
+    static final float SPEED = 2f;
+    static final float SIZE  = 0.5f;
+
+    // Player position within the map
+    Vector2 position = new Vector2();
+    // Used to calculate the speed that the player moves
+    Vector2 velocity = new Vector2();
+    // Bounding box to detect player collision
+    Rectangle bounds = new Rectangle();
+
+    State currentState = State.IDLE;
+    Direction currentDirection = Direction.LEFT;
+
+    public Player(Vector2 position) {
+        this.position = position;
+        this.bounds.height = SIZE;
+        this.bounds.width = SIZE;
+    }
 }
 
