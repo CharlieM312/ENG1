56177a95f0cb0f7c3bc1661930657a70148b89de
msw312 <msweetman99@gmail.com>
2024-03-18 18:38:25 +0000

    Refactor code in GameScreen that checks for input into a method in Player class. Create methods for moving player in set direction

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -12,42 +12,48 @@ import com.mygdx.game.Player.State;
 
 public class GameScreen implements Screen {
     final HeslingtonHustle game;
+
     int upKey, rightKey, downKey, leftKey
     , wKey, dKey, sKey, aKey;
+
     Player player;
-    House house1;
-	StudyBuilding study1;
-	Lake lake1;
-	Restaurant food1;
+    House house;
+	StudyBuilding piazza;
+	Lake lake;
+	Restaurant glasshouse;
 	Texture background;
+
 	String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 	String currentDay;
+
 	int activityCounter;
 	int foodCounter;
 	int dayCounter;
+
     OrthographicCamera camera;
 
     public GameScreen(final HeslingtonHustle game) {
         this.game = game;
 
-        upKey = Input.Keys.UP;
+        upKey    = Input.Keys.UP;
 		rightKey = Input.Keys.RIGHT;
-		downKey = Input.Keys.DOWN;
-		leftKey = Input.Keys.LEFT;
-		wKey = Input.Keys.W;
-		dKey = Input.Keys.D;
-		sKey = Input.Keys.S;
-		aKey = Input.Keys.A;
+		downKey  = Input.Keys.DOWN;
+		leftKey  = Input.Keys.LEFT;
+		wKey     = Input.Keys.W;
+		dKey     = Input.Keys.D;
+		sKey     = Input.Keys.S;
+		aKey     = Input.Keys.A;
 
         camera = new OrthographicCamera();
 		camera.setToOrtho(false, 1000, 600);
 
-        player = new Player(new Vector2(168, 20));
-        house1 = new House();
-        study1 = new StudyBuilding();
-		lake1 = new Lake();
-		food1 = new Restaurant();
+        player     = new Player(new Vector2(168, 20));
+        house      = new House();
+        piazza     = new StudyBuilding();
+		lake       = new Lake();
+		glasshouse = new Restaurant();
 		background = new Texture("background.jpg");
+		
 		dayCounter = 0;
 		foodCounter = 0;
 		activityCounter = 0;
@@ -56,7 +62,7 @@ public class GameScreen implements Screen {
     }
 
     @Override
-    public void render(float felta) {
+    public void render(float delta) {
         ScreenUtils.clear(0, 0, 0, 0);
 		camera.update();
 		game.batch.setProjectionMatrix(camera.combined);
@@ -69,10 +75,10 @@ public class GameScreen implements Screen {
 				, player.GetXPosition(), player.GetYPosition());
 
 		// Draws buildings now using location class
-		game.batch.draw(study1.locationTexture, 400, 280);
-		game.batch.draw(house1.locationTexture, 100, 280);
-		game.batch.draw(lake1.locationTexture, 700, 470);
-		game.batch.draw(food1.locationTexture, 250, 470);
+		game.batch.draw(piazza.locationTexture, 400, 280);
+		game.batch.draw(house.locationTexture, 100, 280);
+		game.batch.draw(lake.locationTexture, 700, 470);
+		game.batch.draw(glasshouse.locationTexture, 250, 470);
 
 		// Draws text
         game.font.getData().setScale(1, 1);
@@ -80,111 +86,27 @@ public class GameScreen implements Screen {
 		game.font.draw(game.batch, "Times Eaten: " + foodCounter, 800, 560);
 		game.font.draw(game.batch, "Day: " + currentDay, 10, 580);
         game.batch.end();
-
-        float currentX = player.GetXPosition();
-		float currentY = player.GetYPosition();
-
-		// If any directional key is pressed, set player state to moving
-		if (Gdx.input.isKeyPressed(upKey)  || Gdx.input.isKeyPressed(rightKey)
-                || Gdx.input.isKeyPressed(downKey) || Gdx.input.isKeyPressed(leftKey)
-                || Gdx.input.isKeyPressed(wKey)    || Gdx.input.isKeyPressed(dKey)
-                || Gdx.input.isKeyPressed(sKey)    || Gdx.input.isKeyPressed(aKey)) 
-		    player.SetState(State.MOVING);
-
-		// Checks for key presses and moves player in specified direction,
-		// otherwise set the texture to idle
-		if ((Gdx.input.isKeyPressed(rightKey) && Gdx.input.isKeyPressed(upKey)) || 
-			(Gdx.input.isKeyPressed(dKey) && Gdx.input.isKeyPressed(wKey))) {
-
-			player.SetDirection(Direction.RIGHT);
-			player.setPosition(new Vector2(currentX += 180 * Gdx.graphics.getDeltaTime()
-							, currentY += 180 * Gdx.graphics.getDeltaTime()));
-		}
-		else if ((Gdx.input.isKeyPressed(rightKey) && Gdx.input.isKeyPressed(downKey)) ||
-		         (Gdx.input.isKeyPressed(dKey) && Gdx.input.isKeyPressed(sKey))) {
-
-			player.SetDirection(Direction.RIGHT);
-			player.setPosition(new Vector2(currentX += 180 * Gdx.graphics.getDeltaTime()
-							, currentY -= 180 * Gdx.graphics.getDeltaTime()));
-		}
-		else if ((Gdx.input.isKeyPressed(leftKey) && Gdx.input.isKeyPressed(upKey)) || 
-		         (Gdx.input.isKeyPressed(aKey) && Gdx.input.isKeyPressed(wKey))) {
-
-			player.SetDirection(Direction.LEFT);
-			player.setPosition(new Vector2(currentX -= 180 * Gdx.graphics.getDeltaTime()
-							, currentY += 180 * Gdx.graphics.getDeltaTime()));
-		}
-		else if ((Gdx.input.isKeyPressed(leftKey) && Gdx.input.isKeyPressed(downKey)) ||
-		         (Gdx.input.isKeyPressed(aKey) && Gdx.input.isKeyPressed(sKey))) {
-
-			player.SetDirection(Direction.LEFT);
-			player.setPosition(new Vector2(currentX -= 180 * Gdx.graphics.getDeltaTime()
-							, currentY -= 180 * Gdx.graphics.getDeltaTime()));
-		}
-		else if (Gdx.input.isKeyPressed(upKey) || Gdx.input.isKeyPressed(wKey)) {
-			player.SetYPosition(currentY += (180 * Gdx.graphics.getDeltaTime()));
-		}
-		 else if (Gdx.input.isKeyPressed(leftKey) || Gdx.input.isKeyPressed(aKey)) {
-			player.SetDirection(Direction.LEFT);
-			player.SetXPosition(currentX -= 180 * Gdx.graphics.getDeltaTime());
-		}
-		else if (Gdx.input.isKeyPressed(rightKey) || Gdx.input.isKeyPressed(dKey)) {
-			player.SetDirection(Direction.RIGHT);
-			player.SetXPosition(currentX += 180 * Gdx.graphics.getDeltaTime());
-		}
-		else if (Gdx.input.isKeyPressed(downKey) || Gdx.input.isKeyPressed(sKey)) {
-			player.SetYPosition(currentY -= 180 * Gdx.graphics.getDeltaTime());
-		}
-		else {
-			player.SetState(State.IDLE);
-		}
+		
+		player.CheckForInput();
 		
 		// Checks if player collides study location
-		if (player.bounds.overlaps(study1.bounds)) {
-			if (Gdx.input.isKeyPressed(upKey) || Gdx.input.isKeyPressed(wKey))
-				player.SetYPosition(currentY - 60) ;
-			else if (Gdx.input.isKeyPressed(leftKey) || Gdx.input.isKeyPressed(aKey))
-				player.SetXPosition(currentX + 60);
-			else if (Gdx.input.isKeyPressed(downKey) || Gdx.input.isKeyPressed(wKey)) 
-				player.SetYPosition(currentY + 60);
-			else if (Gdx.input.isKeyPressed(rightKey) || Gdx.input.isKeyPressed(wKey))
-				player.SetXPosition(currentX - 60);	
+		if (player.bounds.overlaps(piazza.bounds)) {
+			player.SetState(State.LOCKED);
 		}
 
 		// Checks if player collides with food location
-		if (player.bounds.overlaps(food1.bounds)) {
-			if (Gdx.input.isKeyPressed(upKey) || Gdx.input.isKeyPressed(wKey))
-				player.SetYPosition(currentY - 60) ;
-			else if (Gdx.input.isKeyPressed(leftKey) || Gdx.input.isKeyPressed(aKey))
-				player.SetXPosition(currentX + 60);
-			else if (Gdx.input.isKeyPressed(downKey) || Gdx.input.isKeyPressed(wKey)) 
-				player.SetYPosition(currentY + 60);
-			else if (Gdx.input.isKeyPressed(rightKey) || Gdx.input.isKeyPressed(wKey))
-				player.SetXPosition(currentX - 60);
+		if (player.bounds.overlaps(glasshouse.bounds)) {
+			player.SetState(State.LOCKED);
 		}
 		
 		// Checks if player collides with lake location
-		if (player.bounds.overlaps(lake1.bounds)) {
-			if (Gdx.input.isKeyPressed(upKey) || Gdx.input.isKeyPressed(wKey))
-				player.SetYPosition(currentY - 60) ;
-			else if (Gdx.input.isKeyPressed(leftKey) || Gdx.input.isKeyPressed(aKey))
-				player.SetXPosition(currentX + 60);
-			else if (Gdx.input.isKeyPressed(downKey) || Gdx.input.isKeyPressed(wKey)) 
-				player.SetYPosition(currentY + 60);
-			else if (Gdx.input.isKeyPressed(rightKey) || Gdx.input.isKeyPressed(wKey))
-				player.SetXPosition(currentX - 60);
+		if (player.bounds.overlaps(lake.bounds)) {
+			player.SetState(State.LOCKED);
 		}
 
 		// Checks if player collides with house location
-		if (player.bounds.overlaps(house1.bounds)) {
-			if (Gdx.input.isKeyPressed(upKey) || Gdx.input.isKeyPressed(wKey))
-				player.SetYPosition(currentY - 60) ;
-			else if (Gdx.input.isKeyPressed(leftKey) || Gdx.input.isKeyPressed(aKey))
-				player.SetXPosition(currentX + 60);
-			else if (Gdx.input.isKeyPressed(downKey) || Gdx.input.isKeyPressed(wKey)) 
-				player.SetYPosition(currentY + 60);
-			else if (Gdx.input.isKeyPressed(rightKey) || Gdx.input.isKeyPressed(wKey))
-				player.SetXPosition(currentX - 60);
+		if (player.bounds.overlaps(house.bounds)) {
+			player.SetState(State.LOCKED);
 		}
 
 		// Checks if player is within bounds of the map after moving
--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -2,12 +2,15 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 
 public class Player {
 
     public enum State {
         IDLE,
+        LOCKED,
         MOVING,
         DOING_RECREATIONAL_ACTIVITY,
         STUDYING,
@@ -50,7 +53,7 @@ public class Player {
     }
 
     public Texture GetCurrentTexture() {
-        if (currentState == State.IDLE)
+        if (currentState == State.IDLE || currentState == State.LOCKED)
             return idleTexture;
 
         switch (currentDirection) {
@@ -62,6 +65,73 @@ public class Player {
                 return idleTexture;   
         }
     }
+
+    public void MoveUp() {
+        SetYPosition(position.y += (180 * Gdx.graphics.getDeltaTime()));
+    }
+
+    public void MoveRight() {
+        SetDirection(Direction.RIGHT);
+        SetXPosition(position.x += 180 * Gdx.graphics.getDeltaTime());        
+    }
+
+    public void MoveDown() {
+        SetYPosition(position.y -= 180 * Gdx.graphics.getDeltaTime());
+    }
+
+    public void MoveLeft() {
+        SetDirection(Direction.LEFT);
+        SetXPosition(position.x -= 180 * Gdx.graphics.getDeltaTime());
+    }
+
+    public void CheckForInput() {
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)      || Gdx.input.isKeyPressed(Input.Keys.RIGHT)
+		    || Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.LEFT)
+			|| Gdx.input.isKeyPressed(Input.Keys.W)    || Gdx.input.isKeyPressed(Input.Keys.D)
+			|| Gdx.input.isKeyPressed(Input.Keys.S)    || Gdx.input.isKeyPressed(Input.Keys.A)) 
+			SetState(State.MOVING);
+
+        if ((Gdx.input.isKeyPressed(Input.Keys.RIGHT) && Gdx.input.isKeyPressed(Input.Keys.UP)) || 
+            (Gdx.input.isKeyPressed(Input.Keys.D)     && Gdx.input.isKeyPressed(Input.Keys.W))) {
+            SetDirection(Direction.RIGHT);
+            MoveUp();
+            MoveRight();
+        }
+        else if ((Gdx.input.isKeyPressed(Input.Keys.RIGHT) && Gdx.input.isKeyPressed(Input.Keys.DOWN)) ||
+                 (Gdx.input.isKeyPressed(Input.Keys.D)     && Gdx.input.isKeyPressed(Input.Keys.S))) {
+            SetDirection(Direction.RIGHT);
+            MoveRight();
+            MoveDown();
+        }
+        else if ((Gdx.input.isKeyPressed(Input.Keys.LEFT) && Gdx.input.isKeyPressed(Input.Keys.UP)) || 
+                (Gdx.input.isKeyPressed(Input.Keys.A)     && Gdx.input.isKeyPressed(Input.Keys.W))) {
+            SetDirection(Direction.LEFT);
+            MoveLeft();
+            MoveUp();
+        }
+        else if ((Gdx.input.isKeyPressed(Input.Keys.LEFT) && Gdx.input.isKeyPressed(Input.Keys.DOWN)) ||
+                (Gdx.input.isKeyPressed(Input.Keys.A)     && Gdx.input.isKeyPressed(Input.Keys.S))) {
+            SetDirection(Direction.LEFT);
+            MoveLeft();
+            MoveDown();
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.W)) {
+            MoveUp();
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A)) {
+            MoveLeft();
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D)) {
+            MoveRight();
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.S)) {
+            MoveDown();
+        }
+        else {
+            SetState(State.IDLE);
+        }
+    }
+    
 
     public void setPosition(Vector2 position) {
         bounds.setPosition(position);
