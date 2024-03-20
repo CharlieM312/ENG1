f8c913307dc50ded6d3abfaf7120ed5e214cb5f8
CharlieM312 <cm2339@york.ac.uk>
2024-03-20 14:59:21 +0000

    Updated energy modifier

--- core/src/com/mygdx/game/Restaurant.java
+++ core/src/com/mygdx/game/Restaurant.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.math.Vector2;
 // Defines Resturant as a GameLocation
 public class Restaurant extends GameLocation {
     public Restaurant() {
-        energyModifier = 20;
+        energyModifier = -10;
         locationTexture = new Texture("restaurant.jpg");
         position = new Vector2(250, 470);
         bounds.setPosition(position);
