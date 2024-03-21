0d9e76d0b280c09cf38eabf143bbab4dfd047f90
Max Sweetman <msweetman99@gmail.com>
2024-03-21 10:38:51 +0000

    Remove sky from piazza

--- core/src/com/mygdx/game/GameOverScreen.java
+++ core/src/com/mygdx/game/GameOverScreen.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 // Implements Game Over Screen
@@ -15,8 +16,10 @@ public class GameOverScreen implements Screen {
     final HeslingtonHustle game;
     OrthographicCamera camera;
     Texture background;
+    Vector2 titlePosition;
     private float finalscore;
     public GameOverScreen(final HeslingtonHustle game) {
+        titlePosition = new Vector2((game.screenWidth / 2) - (150), (game.screenHeight / 2));
         this.game = game;
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1000, 600);
@@ -34,8 +37,9 @@ public class GameOverScreen implements Screen {
         game.batch.draw(background, 0, 0);
         game.font.getData().setScale(3, 3);
         game.font.setColor(Color.WHITE);
-        game.font.draw(game.batch, "Heslington Hustle", 300, 300);
-        game.font.draw(game.batch, "Your final score was: " + finalscore, 300,250);
+        game.font.draw(game.batch, "Heslington Hustle", titlePosition.x, titlePosition.y);
+        game.font.getData().setScale(2, 2);
+        game.font.draw(game.batch, "Final score: " + finalscore, titlePosition.x + 60, 250);
         game.font.draw(game.batch, "Press M to return to the main menu", 300, 200);
         game.batch.end();
         if (Gdx.input.isKeyJustPressed(Input.Keys.M)) {
